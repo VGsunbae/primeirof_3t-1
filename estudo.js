49f643a1842f356dc3fbf2738cd77418a07e4b2e
ponziot <111065521+ponziot@users.noreply.github.com>
2022-10-20 11:11:13 -0300

    Update estudo.js

--- estudo.js
+++ estudo.js
@@ -43,3 +43,14 @@ function total(){
    
     document.write("Resultado: " + r );
 }
+
+
+function nota(){
+  let n1 = document.getElementById("b1").value;
+  let n2 = document.getElementById("b2").value;
+  let n3 = document.getElementById("b3").value;
+  let n4 = document.getElementById("b4").value;
+  let r = Number(n1)+Number(n2)+Number(n3)+Number(n4);
+
+  document.getElementById("resul").innerHTML = "Nota: " + r;
+}
